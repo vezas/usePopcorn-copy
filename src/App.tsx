7276301536c2094b4cab06a3ba812f1c16cc8a0d
Radek Wrona <radoslaw.wrona1@gmail.com>
2023-12-30 16:46:39 +0100

    feat: create skeleton for movie detail

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import { Box, Layout } from 'lib/components/ui';
 import { MovieItem } from 'lib/components/ui/MovieItem';
+import { MovieList } from 'lib/types';
 import { useEffect, useState } from 'react';
 
 const KEY = 'efab1932';
 
 export const App = () => {
   const [query, setQuery] = useState('');
-  const [movieList, setMovieList] = useState([]);
+  const [movieList, setMovieList] = useState<MovieList[]>([]);
 
   const handleQuery = (query: string) => setQuery(query);
 
   useEffect(() => {
+    if (query.length <= 3) return;
+
     const controller = new AbortController();
 
     const fetchMovieList = async () => {
       const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
         signal: controller.signal
       });
-      const data = await res.json();
-      setMovieList(data.Search);
+      const { Search: data }: { Search: MovieList[] } = await res.json();
+      setMovieList(data || []);
     };
 
     fetchMovieList();
@@ -30,7 +33,7 @@ export const App = () => {
     <Layout handleQuery={handleQuery}>
       <Box>
         {movieList.map((movie) => (
-          <MovieItem movie={movie} />
+          <MovieItem key={movie.imdbID} movie={movie} />
         ))}
       </Box>
       <Box>box2</Box>
--- src/lib/components/ui/MovieItem/MovieItem.tsx
+++ src/lib/components/ui/MovieItem/MovieItem.tsx
@@ -1,13 +1,20 @@
 import { FC } from 'react';
+import { MovieList } from 'lib/types';
+import NoPhotoIcon from 'assets/no-image.svg?react';
+import styles from './MovieItem.module.scss';
 
 interface MovieItemProps {
-  movie: [];
+  movie: MovieList;
 }
 
-export const MovieItem: FC<MovieItemProps> = () => {
+export const MovieItem: FC<MovieItemProps> = ({ movie }) => {
+  const { Poster: poster, Title: title, Year: year } = movie;
+
   return (
-    <div>
-      <img src='' alt='' />
+    <div className={styles.movieItem}>
+      {poster !== 'N/A' ? <img src={poster} alt='' /> : <NoPhotoIcon />}
+      <h2>{title}</h2>
+      <p>{year}</p>
     </div>
   );
 };
